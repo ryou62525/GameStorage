1c26b9d1d67d6f839e3da5225c1fcf29f58430da
ryou62525 <rkc_tw_st@yahoo.co.jp>
2016-01-13 10:11:01 +0900

    ヘッダーファイル追加

---
+++ craft_work02Ver.0.0.0/main.cpp
@@ -0,0 +1,27 @@
+#include "src/scene/sceneManager.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmaLine, int  nCmaShow)
+{
+	if (DxLib_Init() == -1)return -1;
+	ChangeWindowMode(TRUE);
+	SetGraphMode(640, 480, GetColor(0, 0, 0));
+
+	DrawBox(0, 0, 32, 32, GetColor(255, 255, 255), TRUE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	WaitKey();
+	DxLib_End();
+	return 0;
+}
